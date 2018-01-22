c6ae1f972f4ed888fc1fe679f6146e67f903de7d
Michael Magrian <michael.magrian@gmail.com>
2018-01-22 13:45:32 +0100

    Change about view

--- src/router/AboutView.vue
+++ src/router/AboutView.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    <img src="./../../static/img/home.jpg" />
+    <h1>About this Page</h1>
+    <div class="mdl-grid">
+      <div class="mdl-cell mdl-cell--9-col mdl-cell--10-col--phone mdl-cell--8-col--tablet mdl-cell--9-col--desktop">
+        <p>Lorem Ipsum</p>
+      </div>
+      <div class="mdl-cell mdl-cell--3-col mdl-cell--10-col--phone mdl-cell--4-col--tablet mdl-cell--3-col--desktop">
+        <img class="portrait-image" src="./../../static/img/portrait.jpg" />
+      </div>
+    </div>
+  
   </div>
 </template>
 
@@ -10,5 +19,8 @@ export default {
 </script>
 
 <style scoped>
-
+  .portrait-image {
+    max-width: 230px;  
+    border-radius: 50%;
+  }
 </style>
