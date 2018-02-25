382a55fca95d0dda3ba00745b00733e9e8ade295
Michael Magrian <michael.magrian@gmail.com>
2018-02-25 21:45:18 +0100

    Changing map display style

--- src/router/ExhibitionView.vue
+++ src/router/ExhibitionView.vue
@@ -19,17 +19,17 @@
   .map {
     overflow: hidden;
   }
-
   .map__image {
     position: absolute;
     top: 0;
     left: 0;
-    height: 100vh;
+    width: 100vw;
+    height: calc(100vh - 100px);
     white-space: nowrap;
     overflow: scroll;
     -webkit-overflow-scrolling: touch;
+    overflow-y: scroll
   }
-
   .map__image img {
     width: auto;
     height: 100%;
