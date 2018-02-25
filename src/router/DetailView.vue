ac21508bd056c5f69db9b96c40dc5a311eb6a502
Michael Magrian <michael.magrian@gmail.com>
2018-02-25 21:45:04 +0100

    Adding dynamic detail view

--- src/router/DetailView.vue
+++ src/router/DetailView.vue
@@ -1,51 +1,68 @@
 <template>
   <div class="mdl-grid">
-    <div class="mdl-cell mdl-cell--8-col">
-      <div class="info">
-        <span>{{ cat.info }}</span>
-      </div>
+    <div class="mdl-cell">
+      <img v-bind:src="img" alt="avatar" />
+      <h3>Meet <span>{{ this.name }}</span></h3>
+      <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
     </div>
-    <div class="mdl-cell mdl-cell--4-col mdl-cell--8-col-tablet">
-      <div class="comment">
-        <span>{{ cat.comment }}</span>
-      </div>
-      <div class="actions">
-        <router-link class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored" to="/post">
-          ANSWER
-        </router-link>
-      </div>
-    </div>
+    <section>
+      <img src="" alt="">
+    </section>
   </div>
 </template>
 <script>
-  import { find } from 'lodash'
   export default {
     data () {
       return {
-        cat: null
+        speaker: [
+          {
+            id: '12345',
+            name: 'Frank Furt',
+            imgSrc: '/static/img/avatar_1.jpeg'
+          },
+          {
+            id: '23456',
+            name: 'Sabine Biene',
+            imgSrc: '/static/img/avatar_2.jpeg'
+          },
+          {
+            id: '34567',
+            name: 'Tina Troll',
+            imgSrc: '/static/img/avatar_3.jpeg'
+          },
+          {
+            id: '45678',
+            name: 'Wolfgang Wuff',
+            imgSrc: '/static/img/avatar_4.jpeg'
+          },
+          {
+            id: '56789',
+            name: 'Lukas Lustig',
+            imgSrc: '/static/img/avatar_5.jpeg'
+          }
+        ],
+        name: null,
+        img: null
       }
     },
-    mounted () {
-      this.cat = find(this.$root.cat, (cat) => cat['.key'] === this.$route.params.id)
+    created () {
+      let injectSpeaker = () => {
+        console.log(this.$route.params.id)
+        let currentID = this.$route.params.id.toString()
+        console.log(currentID)
+        this.speaker.forEach(element => {
+          if (element.id === currentID) {
+            this.name = element.name
+            this.img = element.imgSrc
+          }
+        })
+      }
+      injectSpeaker()
     }
   }
 </script>
 <style scoped>
-  .picture > img {
-    color: #fff;
-    width:100%;
-  }
-  .info {
-    text-align: right;
-    padding: 5px;
-    color: #555;
-    font-size: 10px;
-  }
-  .comment {
-    padding: 10px;
-    color: #555;
-  }
-  .actions {
+  .mdl-cell {
     text-align: center;
   }
 </style>
